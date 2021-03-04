c1763760722d04608a2d775c2809e1ddc7f48842
SliverHorn <sliver_horn@qq.com>
2021-03-04 15:48:49 +0800

    update gva

---
+++ cmd/gva/internal/model/sys_casbin.go
@@ -0,0 +1,12 @@
+package model
+
+type Casbin struct {
+	Path        string `gorm:"column:v1"`
+	PType       string `gorm:"column:p_type"`
+	Method      string `gorm:"column:v2"`
+	AuthorityId string `gorm:"column:v0"`
+}
+
+func (c *Casbin) TableName() string {
+	return "casbin_rule"
+}
